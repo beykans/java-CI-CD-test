613c31cfdc3b0c1415c2f453a6b94c6eb1e3e6dd
acer <beykan_sevdar@hotmail.com>
2024-10-08 20:46:55 +0300

    Correcting the operations

--- main/math-operations/src/main/java/com/example/demo/FaultyMathOperations.java
+++ main/math-operations/src/main/java/com/example/demo/FaultyMathOperations.java
@@ -2,10 +2,10 @@ package com.example.demo;
 
 public class FaultyMathOperations {
     public int add(int a, int b) {
-        return a - b; // Intentional mistake: should be addition
+        return a / b; // Intentional mistake: should be addition
     }
 
     public int multiply(int a, int b) {
-        return a / b; // Intentional mistake: should be multiplication
+        return a * b; // Intentional mistake: should be multiplication
     }
 }
